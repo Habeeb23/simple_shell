4953ed0bc37ce939cefd93167f0775f5ca3c4c7b
Habeeb23 <113600396+Habeeb23@users.noreply.github.com>
2022-12-13 02:40:05 +0800

    Update getLine.c

--- getLine.c
+++ getLine.c
@@ -1,147 +1,170 @@
 #include "shell.h"
 
-
-
 /**
+ * input_buf - buffers chained commands
+ * @info: parameter struct
+ * @buf: address of buffer
+ * @len: address of len var
  *
- *  * interactive - returns true if shell is interactive mode
- *
- *   * @info: struct address
- *
- *    *
- *
- *     * Return: 1 if interactive mode, 0 otherwise
- *
- *      */
-
-int interactive(info_t *info)
-
+ * Return: bytes read
+ */
+ssize_t input_buf(info_t *info, char **buf, size_t *len)
 {
+	ssize_t r = 0;
+	size_t len_p = 0;
 
-		return (isatty(STDIN_FILENO) && info->readfd <= 2);
-
+	if (!*len) /* if nothing left in the buffer, fill it */
+	{
+		/*bfree((void **)info->cmd_buf);*/
+		free(*buf);
+		*buf = NULL;
+		signal(SIGINT, sigintHandler);
+#if USE_GETLINE
+		r = getline(buf, &len_p, stdin);
+#else
+		r = _getline(info, buf, &len_p);
+#endif
+		if (r > 0)
+		{
+			if ((*buf)[r - 1] == '\n')
+			{
+				(*buf)[r - 1] = '\0'; /* remove trailing newline */
+				r--;
+			}
+			info->linecount_flag = 1;
+			remove_comments(*buf);
+			build_history_list(info, *buf, info->histcount++);
+			/* if (_strchr(*buf, ';')) is this a command chain? */
+			{
+				*len = r;
+				info->cmd_buf = buf;
+			}
+		}
+	}
+	return (r);
 }
 
-
-
 /**
+ * get_input - gets a line minus the newline
+ * @info: parameter struct
  *
- *  * is_delim - checks if character is a delimeter
- *
- *   * @c: the char to check
- *
- *    * @delim: the delimeter string
- *
- *     * Return: 1 if true, 0 if false
- *
- *      */
-
-int is_delim(char c, char *delim)
-
+ * Return: bytes read
+ */
+ssize_t get_input(info_t *info)
 {
+	static char *buf; /* the ';' command chain buffer */
+	static size_t i, j, len;
+	ssize_t r = 0;
+	char **buf_p = &(info->arg), *p;
 
-		while (*delim)
+	_putchar(BUF_FLUSH);
+	r = input_buf(info, &buf, &len);
+	if (r == -1) /* EOF */
+		return (-1);
+	if (len)	/* we have commands left in the chain buffer */
+	{
+		j = i; /* init new iterator to current buf position */
+		p = buf + i; /* get pointer for return */
 
-					if (*delim++ == c)
+		check_chain(info, buf, &j, i, len);
+		while (j < len) /* iterate to semicolon or end */
+		{
+			if (is_chain(info, buf, &j))
+				break;
+			j++;
+		}
 
-									return (1);
+		i = j + 1; /* increment past nulled ';'' */
+		if (i >= len) /* reached end of buffer? */
+		{
+			i = len = 0; /* reset position and length */
+			info->cmd_buf_type = CMD_NORM;
+		}
 
-			return (0);
+		*buf_p = p; /* pass back pointer to current command position */
+		return (_strlen(p)); /* return length of current command */
+	}
 
+	*buf_p = buf; /* else not a chain, pass back buffer from _getline() */
+	return (r); /* return length of buffer from _getline() */
 }
 
-
-
 /**
+ * read_buf - reads a buffer
+ * @info: parameter struct
+ * @buf: buffer
+ * @i: size
  *
- *  *_isalpha - checks for alphabetic character
- *
- *   *@c: The character to input
- *
- *    *Return: 1 if c is alphabetic, 0 otherwise
- *
- *     */
-
-
-
-int _isalpha(int c)
-
+ * Return: r
+ */
+ssize_t read_buf(info_t *info, char *buf, size_t *i)
 {
+	ssize_t r = 0;
 
-		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-
-					return (1);
-
-			else
-
-						return (0);
-
+	if (*i)
+		return (0);
+	r = read(info->readfd, buf, READ_BUF_SIZE);
+	if (r >= 0)
+		*i = r;
+	return (r);
 }
 
+/**
+ * _getline - gets the next line of input from STDIN
+ * @info: parameter struct
+ * @ptr: address of pointer to buffer, preallocated or NULL
+ * @length: size of preallocated ptr buffer if not NULL
+ *
+ * Return: s
+ */
+int _getline(info_t *info, char **ptr, size_t *length)
+{
+	static char buf[READ_BUF_SIZE];
+	static size_t i, len;
+	size_t k;
+	ssize_t r = 0, s = 0;
+	char *p = NULL, *new_p = NULL, *c;
 
+	p = *ptr;
+	if (p && length)
+		s = *length;
+	if (i == len)
+		i = len = 0;
+
+	r = read_buf(info, buf, &len);
+	if (r == -1 || (r == 0 && len == 0))
+		return (-1);
+
+	c = _strchr(buf + i, '\n');
+	k = c ? 1 + (unsigned int)(c - buf) : len;
+	new_p = _realloc(p, s, s ? s + k : k + 1);
+	if (!new_p) /* MALLOC FAILURE! */
+		return (p ? free(p), -1 : -1);
+
+	if (s)
+		_strncat(new_p, buf + i, k - i);
+	else
+		_strncpy(new_p, buf + i, k - i + 1);
+
+	s += k - i;
+	i = k;
+	p = new_p;
+
+	if (length)
+		*length = s;
+	*ptr = p;
+	return (s);
+}
 
 /**
+ * sigintHandler - blocks ctrl-C
+ * @sig_num: the signal number
  *
- *  *_atoi - converts a string to an integer
- *
- *   *@s: the string to be converted
- *
- *    *Return: 0 if no numbers in string, converted number otherwise
- *
- *     */
-
-
-
-int _atoi(char *s)
-
+ * Return: void
+ */
+void sigintHandler(__attribute__((unused))int sig_num)
 {
-
-		int i, sign = 1, flag = 0, output;
-
-			unsigned int result = 0;
-
-
-
-				for (i = 0;  s[i] != '\0' && flag != 2; i++)
-
-						{
-
-									if (s[i] == '-')
-
-													sign *= -1;
-
-
-
-											if (s[i] >= '0' && s[i] <= '9')
-
-														{
-
-																		flag = 1;
-
-																					result *= 10;
-
-																								result += (s[i] - '0');
-
-																										}
-
-													else if (flag == 1)
-
-																	flag = 2;
-
-														}
-
-
-
-					if (sign == -1)
-
-								output = -result;
-
-						else
-
-									output = result;
-
-
-
-							return (output);
-
+	_puts("\n");
+	_puts("$ ");
+	_putchar(BUF_FLUSH);
 }
